4ece99c10b20cda15c8844ff586b62a57ab58f6b
Matias <matiasandinoma@gmail.com>
2024-06-30 11:35:03 -0300

    pequeños cambios visuales en las vistas login y registro

--- src/features/home/views/login_view.jsx
+++ src/features/home/views/login_view.jsx
@@ -7,7 +7,7 @@ const LoginView = () => {
   return (
     <>
       <div className="d-flex vh-100 align-items-center">
-        <div className="container border rounded col-auto p-5 bg-custom-opacity text-light min-width-25" >
+        <div className="container border border-secondary border-2 rounded col-auto p-5 bg-dark bg-opacity-100 text-light min-width-25" >
           <h2 className='text-center mb-5'><strong>INICIAR SESIÓN</strong></h2>
           <LoginForm />
           <p className='mt-4'>No tienes una cuenta? <a className="link-opacity-100" role="button" onClick={() => navigate('/registro')}>Registraté</a></p>
--- src/features/registro/views/registro_view.jsx
+++ src/features/registro/views/registro_view.jsx
@@ -6,7 +6,8 @@ const RegistroView = () => {
   const navigate = useNavigate();
   return (
     <div className="d-flex vh-100 align-items-center">
-      <div className="container border rounded col-auto p-5 bg-custom-opacity text-light min-width-25" >
+      <div className="container border rounded col-auto border-secondary border-2 p-5 bg-dark bg-opacity-100 text-light min-width-25" >
+
         <h2 className='text-center mb-5'><strong>CREAR NUEVA CUENTA</strong></h2>
         <RegistroForm />
         <p className='mt-4'>Ya tienes una cuenta? <a className="link-opacity-100 pointer" role="button" onClick={() => navigate('/')}>Logín</a></p>
